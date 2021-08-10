f661408e0544a98f68a52b97038c902e4d3e92c2
WonderCRM <46189077+WonderCRM@users.noreply.github.com>
2021-08-10 10:57:21 +0300

    Update ui.cpp

--- src/ui.cpp
+++ src/ui.cpp
@@ -14,19 +14,19 @@
   } Select;
 */
 void CRMui3::select(Select item) {
-  if (var(item.id) == "null") var(item.id, item.defaultValue);
+  if (var(item.id) == F("null")) var(item.id, item.defaultValue);
   if (_start) return;
   if (_buf.endsWith("]")) _buf[_buf.length() - 1] = ',';
-  _buf += "{\"o\":[";
+  _buf += F("{\"o\":[");
   for (uint8_t i = 0; i < 50; i++) {
     if (item.options[i][0] != "") {
       if (!_buf.endsWith(",") && !_buf.endsWith("[")) _buf += ",";
-      _buf += "[\"" + item.options[i][0] + "\",\"" + item.options[i][1] + "\"]";
+      _buf += String() + F("[\"") + item.options[i][0] + F("\",\"") + item.options[i][1] + F("\"]");
     } else break;
   }
-  _buf += "],\"id\":\"" + item.id + "\",";
-  _buf += "\"t\":" + String(INPUT_SELECT) + ",";
-  _buf += "\"l\":\"" + item.label + "\"}]";
+  _buf += String() + F("],\"id\":\"") + item.id + F("\",");
+  _buf += String() + F("\"t\":") + String(INPUT_SELECT) + ",";
+  _buf += String() + F("\"l\":\"") + item.label + F("\"}]");
 }
 
 
@@ -41,15 +41,17 @@ void CRMui3::select(Select item) {
   } Input;
 */
 void CRMui3::input(Input item) {
-  if (item.type != INPUT_BUTTON && item.type != INPUT_WIFI && var(item.id) == "null") var(item.id, item.defaultValue);
+  if (item.type != INPUT_BUTTON && item.type != INPUT_WIFI && var(item.id) == F("null"))
+    var(item.id, item.defaultValue);
   if (_start) return;
   if (_buf.endsWith("]")) _buf[_buf.length() - 1] = ',';
-  _buf += "{\"t\":" + String(item.type);
-  _buf += ",\"id\":\"" + item.id + "\"";
-  if (item.d != "") _buf += ",\"d\":\"" + item.d + "\"";
-  if (item.s != "") _buf += ",\"s\":" + item.s;
-  if (item.type == INPUT_BUTTON && item.defaultValue != "") _buf += ",\"p\":\"" + item.defaultValue + "\"";
-  _buf += ",\"l\":\"" + item.label + "\"}]";
+  _buf += String() + F("{\"t\":") + String(item.type);
+  _buf += String() + F(",\"id\":\"") + item.id + "\"";
+  if (item.d != "") _buf += String() + F(",\"d\":\"") + item.d + "\"";
+  if (item.s != "") _buf += String() + F(",\"s\":") + item.s;
+  if (item.type == INPUT_BUTTON && item.defaultValue != "")
+    _buf += String() + F(",\"p\":\"") + item.defaultValue + "\"";
+  _buf += String() + F(",\"l\":\"") + item.label + F("\"}]");
 }
 
 
@@ -66,12 +68,12 @@ void CRMui3::input(Input item) {
 void CRMui3::output(Output item) {
   if (_start) return;
   if (_buf.endsWith("]")) _buf[_buf.length() - 1] = ',';
-  _buf += "{\"t\":" + String(item.type);
-  _buf += ",\"id\":\"" + item.id + "\"";
-  _buf += ",\"l\":\"" + item.label + "\"";
-  _buf += ",\"d\":\"" + item.def + "\"";
-  if (item.c != "") _buf += ",\"c\":\"" + item.c + "\"";
-  _buf += "}]";
+  _buf += String() + F("{\"t\":") + String(item.type);
+  _buf += String() + F(",\"id\":\"") + item.id + "\"";
+  _buf += String() + F(",\"l\":\"") + item.label + "\"";
+  _buf += String() + F(",\"d\":\"") + item.def + "\"";
+  if (item.c != "") _buf += String() + F(",\"c\":\"") + item.c + "\"";
+  _buf += F("}]");
 }
 
 
@@ -87,16 +89,16 @@ void CRMui3::output(Output item) {
   } Range;
 */
 void CRMui3::range(Range item) {
-  if (var(item.id) == "null") var(item.id, item.defaultValue);
+  if (var(item.id) == F("null")) var(item.id, item.defaultValue);
   if (_start) return;
   if (_buf.endsWith("]")) _buf[_buf.length() - 1] = ',';
-  _buf += "{\"t\":" + String(INPUT_RANGE) + ",";
-  _buf += "\"id\":\"" + item.id + "\",";
-  _buf += "\"mn\":" + String(item.min) + ",";
-  _buf += "\"mx\":" + String(item.max) + ",";
-  _buf += "\"s\":" + String(item.step) + ",";
-  _buf += "\"u\":\"" + item.unit + "\",";
-  _buf += "\"l\":\"" + item.label + "\"}]";
+  _buf += String() + F("{\"t\":") + String(INPUT_RANGE) + ",";
+  _buf += String() + F("\"id\":\"") + item.id + F("\",");
+  _buf += String() + F("\"mn\":") + String(item.min) + ",";
+  _buf += String() + F("\"mx\":") + String(item.max) + ",";
+  _buf += String() + F("\"s\":") + String(item.step) + ",";
+  _buf += String() + F("\"u\":\"") + item.unit + F("\",");
+  _buf += String() + F("\"l\":\"") + item.label + F("\"}]");
 }
 
 
@@ -112,17 +114,19 @@ void CRMui3::range(Range item) {
   } Card;
 */
 void CRMui3::card(Card item) {
-  if (item.type == CARD_CHECKBOX && var(item.id) == "null") var(item.id, item.defaultValue);
+  if (item.type == CARD_CHECKBOX && var(item.id) == F("null"))
+    var(item.id, item.defaultValue);
   if (_start) return;
   if (_buf.endsWith("]")) _buf[_buf.length() - 1] = ',';
-  _buf += "{\"t\":" + String(item.type);
-  _buf += ",\"id\":\"" + item.id + "\"";
-  _buf += ",\"l\":\"" + item.label + "\"";
-  if (item.type != CARD_CHECKBOX) _buf += ",\"v\":\"" + item.defaultValue + "\"";
-  if (item.icon != "") _buf += ",\"i\":\"" + item.icon + "\"";
-  if (item.color != "") _buf += ",\"c\":\"" + item.color + "\"";
-  if (item.newGroup) _buf += ",\"n\":1";
-  _buf += "}]";
+  _buf += String() + F("{\"t\":") + String(item.type);
+  _buf += String() + F(",\"id\":\"") + item.id + "\"";
+  _buf += String() + F(",\"l\":\"") + item.label + "\"";
+  if (item.type != CARD_CHECKBOX)
+    _buf += String() + F(",\"v\":\"") + item.defaultValue + "\"";
+  if (item.icon != "") _buf += String() + F(",\"i\":\"") + item.icon + "\"";
+  if (item.color != "") _buf += String() + F(",\"c\":\"") + item.color + "\"";
+  if (item.newGroup) _buf += String() + F(",\"n\":1");
+  _buf += F("}]");
 }
 
 
@@ -140,14 +144,14 @@ void CRMui3::card(Card item) {
 void CRMui3::chart(Chart item) {
   if (_start) return;
   if (_buf.endsWith("]")) _buf[_buf.length() - 1] = ',';
-  _buf += "{\"t\":" + String(item.type);
-  _buf += ",\"id\":\"" + item.id + "\"";
-  _buf += ",\"l\":\"" + item.label + "\"";
-  if (item.labelLine != "") _buf += ",\"a\":" + item.labelLine + "";
-  if (item.valueLine != "") _buf += ",\"b\":" + item.valueLine + "";
-  if (item.color != "") _buf += ",\"c\":\"" + item.color + "\"";
-  if (item.height != "") _buf += ",\"h\":\"" + item.height + "\"";
-  _buf += "}]";
+  _buf += String() + F("{\"t\":") + String(item.type);
+  _buf += String() + F(",\"id\":\"") + item.id + "\"";
+  _buf += String() + F(",\"l\":\"") + item.label + "\"";
+  if (item.labelLine != "") _buf += String() + F(",\"a\":") + item.labelLine + "";
+  if (item.valueLine != "") _buf += String() + F(",\"b\":") + item.valueLine + "";
+  if (item.color != "") _buf += String() + F(",\"c\":\"") + item.color + "\"";
+  if (item.height != "") _buf += String() + F(",\"h\":\"") + item.height + "\"";
+  _buf += F("}]");
 }
 
 
@@ -157,12 +161,12 @@ void CRMui3::wifiForm(uint8_t mode, const String &ap_ssid, const String &ap_pass
     return;
   }
   if (_buf.endsWith("]")) _buf[_buf.length() - 1] = ',';
-  _buf += "{\"t\":" + String(INPUT_WIFI) + "}]";
+  _buf += String() + F("{\"t\":") + String(INPUT_WIFI) + F("}]");
 }
 
 
 void CRMui3::page(const String &pageName) {
   if (_start) return;
   if (!_buf.endsWith(",") && !_buf.endsWith("[")) _buf += ",";
-  _buf += "[{\"m\":\"" + pageName + "\"}]";
+  _buf += String() + F("[{\"m\":\"") + pageName + F("\"}]");
 }
